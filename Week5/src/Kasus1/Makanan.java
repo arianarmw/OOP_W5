11692af665abc0490549ba9a30a8cd2afda0f520
unknown <arianarahmawati0910@gmail.com>
2022-09-27 21:54:03 +0700

    Mengerjakan class Main

---
+++ Week5/src/Kasus1/Makanan.java
@@ -0,0 +1,46 @@
+/* Author : Ariana Rahmawati
+ * Date   : September 27th 2022
+ * Lesson : Object, Class, Encapsulation
+ * Course : Object Oriented Programming (OOP)
+ * Program ini merupakan hasil revisi dan pengembangan dari modul yang diberikan oleh dosen
+ */
+
+package Kasus1;
+
+public class Makanan {
+	
+	private String[] nama_makanan;
+	private double[] harga_makanan;
+	private int [] stok;
+	
+	Makanan() {
+		this.nama_makanan = new String[10];
+		this.harga_makanan = new double[10];
+		this.stok = new int [10];
+	}
+
+	public String getNama_makanan(int id) {
+		return nama_makanan[id];
+	}
+
+	public void setNama_makanan(String nama_makanan, int id) {
+		this.nama_makanan[id] = nama_makanan;
+	}
+
+	public double getHarga_makanan(int id) {
+		return harga_makanan[id];
+	}
+
+	public void setHarga_makanan(double harga_makanan, int id)  {
+		this.harga_makanan[id] = harga_makanan;
+	}
+
+	public int getStok(int id) {
+		return stok[id];
+	}
+
+	public void setStok(int stok, int id) {
+		this.stok[id] = stok;
+	}	
+
+}
